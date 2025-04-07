b9c89f7682246fab311d56f3f0ff5468cefebfff
lolaop5047 <kthh9828@naver.com>
2025-04-07 21:21:09 +0900

    WIP: “Enemy Script 수정”

--- Assets/01_Scripts/EnemyData/EnemyData.cs
+++ Assets/01_Scripts/EnemyData/EnemyData.cs
@@ -2,18 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-[CreateAssetMenu(fileName = "NewEnemyData", menuName = "Enemy/EnemyData")]
-
+[CreateAssetMenu(fileName = "NewEnemy Data", menuName = "Enemy/EnemyData")] //제일 아래 배치
 public class EnemyData : ScriptableObject
 {
-    public string IDNum; //고유번호
-    public string EnemyName; //캐릭터이름
-    public Sprite Icon; //Icon
-    public int EnemyHealth; //체력
-    public int EnemyATK; //공격력
+    [SerializeField]
+    public string iDNum; //고유번호
+    public string IDNum { get { return iDNum; } } //이름
 
-    [Header("statValue")]
-    public int[] HealthValue; //체력변화
-    public int[] ATKValue; //공격력 변화
+    [SerializeField]
+    private string EnemyName;
+    public string enemyName { get { return enemyName; } } //이름
+    [SerializeField]
+
+    private int hp;
+    public int Hp { get { return hp; } } //체력
+    [SerializeField]
+    private float healthValue;
+    public float HealthValue { get { return healthValue; } } //체력변화
+    [SerializeField]
+    private int aTK;
+    public int ATK { get { return aTK; } } //공격력
+    [SerializeField]
+    private int aTKValue;
+    public int ATKValue { get { return aTKValue; } } //공격력 변화
+    [SerializeField]
+    private float dEF;
+    public float DEF { get { return dEF; } } // 방어력
+    [SerializeField]
+    private float dEFValue;
+    public float DEFValue { get { return dEFValue; } } // 방어력 변화
+
+    [System.Serializable]
+    public enum StencType
+    {
+        High, //상단
+        Middle, //중단
+        Low //하단
+    }
+
+    [System.Serializable]
+    public class StancValue
+    {
+        public float defenseBonus; //방어력 보너스
+        public float attackBonus; //공격력 보너스
+
+    }
+
+    public StencType currentStance;
+
+
 }
