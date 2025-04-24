d9521dfb4ef970d2d5cecbed68a81d89dc6a8eed
alsoox <alsoox173@gmail.com>
2025-04-24 14:46:34 +0900

    [Feat] CardChange

---
+++ Assets/01_Scripts/UI/Camp/CampCardBook.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class CampCardBook : PopupUI_Book
+{
+    public CharacterClass Character { get; set; }
+
+    private void Start()
+    {
+        SetBook(Character);
+    }
+
+    private void SetBook(CharacterClass characterClass)
+    {
+        switch (characterClass)
+        {
+            case CharacterClass.Sophia:
+                OnClick0();
+                break;
+            case CharacterClass.Kayla:
+                OnClick1();
+                break;
+            case CharacterClass.Leon:
+                OnClick2();
+                break;
+        }
+    }
+}
