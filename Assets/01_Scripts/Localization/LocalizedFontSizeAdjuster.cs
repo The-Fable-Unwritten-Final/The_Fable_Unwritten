9d3cde3352037ef690e09dff2bf0c6e6b6a3cb49
kim minjun <k980128@naver.com>
2026-04-09 23:40:14 +0900

    [Feat] Font added with custom letter bold setting added.
    - 신규 폰트 추가(한글,영어 대응)
    - 신규 폰트를 일부 UI 및 카드 텍스트에 적용
    - 일부 UI의 경우 볼드체 유무를 언어에 따라서 조정 가능한 커스텀 스크립트 추가

--- Assets/01_Scripts/Localization/LocalizedFontSizeAdjuster.cs
+++ Assets/01_Scripts/Localization/LocalizedFontSizeAdjuster.cs
@@ -44,22 +44,26 @@ public class LocalizedFontSizeAdjuster : MonoBehaviour
             case "ko":
                 _text.fontSize = fontSize_KO;
                 _text.characterSpacing = 0f;
+                _text.lineSpacing = -10f;
                 _text.wordSpacing = 0f;
                 break;
             case "ja":
                 _text.fontSize = fontSize_JA;
                 _text.characterSpacing = 0f;
+                _text.lineSpacing = 0f;
                 _text.wordSpacing = 0f;
                 break;
             case "en":
                 _text.fontSize = fontSize_EN;
                 _text.characterSpacing = -3f; // 영어 글자 간격 줄이기
+                _text.lineSpacing = -15f; // 영어 줄 간격 줄이기
                 _text.wordSpacing = -8f; // 영어 글자 간격 줄이기
                 break;
             default:
                 // 디폴트 영어
                 _text.fontSize = fontSize_EN;
                 _text.characterSpacing = -3f; // 영어 글자 간격 줄이기
+                _text.lineSpacing = -15f; // 영어 줄 간격 줄이기
                 _text.wordSpacing = -8f; // 영어 글자 간격 줄이기
                 break;
         }
