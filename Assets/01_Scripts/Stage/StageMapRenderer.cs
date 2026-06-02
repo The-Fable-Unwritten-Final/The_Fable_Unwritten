65aedb0e26dcbfe91ce6de866ec9dcf998fab8ad
kim minjun <k980128@naver.com>
2026-06-02 21:49:26 +0900

    [Fix] Stage node button colorset change
    - 스테이지 선택 노드 버튼의 상태별 색상 조절 변경 (책 전체의 등불 쉐이더 적용에 따른 색상 변경에 맞춰서 수정)

--- Assets/01_Scripts/Stage/StageMapRenderer.cs
+++ Assets/01_Scripts/Stage/StageMapRenderer.cs
@@ -83,6 +83,7 @@ public class StageMapRenderer : MonoBehaviour
                 if(node.type == NodeType.Boss)
                 {
                     rt.anchoredPosition = new Vector2(BossNodeXPos, rt.anchoredPosition.y); // 보스 노드 위치 조정
+                    go.GetComponent<Button>().image.color = new Color(42f/255f, 23f/255f, 7f/255f); // 2A1707 색상
                 }
             }
         }
@@ -330,17 +331,25 @@ public class StageMapRenderer : MonoBehaviour
             {
                 btn.enabled = true;
                 btn.interactable = true;
-                btn.image.color = Color.white;
+                // 보스 노드는 색상 변경 안 함 (Render에서 설정한 2A1707 유지)
+                if (node.type != NodeType.Boss)
+                {
+                    btn.image.color = new Color(255f/255f, 187f/255f, 129f/255f);
+                }
                 stageNode.HideClearCircle();
                 stageNode.PlayPulse();
             }
             // 그 외 노드 버튼 비활성화, 색상 흐리게
             else
             {
-                //btn.enabled = false;
+                btn.enabled = true;
                 btn.interactable = false;
                 //btn.image.color = new Color(1, 1, 1, 0.6f); 버튼 노드 불투명도 제거 요청
-                btn.image.color = Color.white;
+                // 보스 노드는 색상 변경 안 함 (Render에서 설정한 2A1707 유지)
+                if (node.type != NodeType.Boss)
+                {
+                    btn.image.color = new Color(255f/255f, 187f/255f, 129f/255f);
+                }
                 stageNode.HideClearCircle();
                 stageNode.StopPulse();
             }
